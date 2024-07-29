1cc7be98424b8abfc4901fcb510fdc8eb9f016e2
Matheus Formentin <formentinmatheus1@gmail.com>
2024-07-29 18:32:38 -0300

    Upload do Projeto

---
+++ react-axios/src/routes/EditPost.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+import blogFetch from '../axios/config';
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
+const EditPost = () => {
+    const navigate = useNavigate()
+
+    const [title, setTitle] = useState()
+    const [body, setBody] = useState()
+
+    const {id} = useParams()
+
+    const getPost = async() => {
+        try {
+            const response = await blogFetch.get(`/posts/${id}`)
+ 
+            const data = response.data
+
+            setTitle(data.title)
+            setBody(data.body)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const editPost = async(e) => {
+        e.preventDefault()
+
+        const post = {title, body, userId: 1}
+
+        await blogFetch.put(`/posts/${id}`, {
+            body: post,
+        });
+    }
+
+    useEffect(() => {
+        getPost();
+    },[])
+
+
+  return (
+    <div className='new-post'>
+    <h2>Editando: {title}</h2>
+    <form onSubmit={(e) => editPost(e)}> 
+      <div className="form-control">
+        <label htmlFor="title">Título:</label>
+        <input type="text" name='title' id='title' placeholder='Digite o Titulo' onChange={(e) => setTitle(e.target.value)} value={title || ""}/>
+        <label htmlFor="body">Conteudo:</label>
+        <textarea name="body" id="body" placeholder='Digite o Conteudo' onChange={(e) => setBody(e.target.value)} value={title || ""} ></textarea>
+      </div>
+      <input type="submit" value="Editar Post" className='btn' />
+    </form>
+  </div>
+  )
+}
+
+export default EditPost
